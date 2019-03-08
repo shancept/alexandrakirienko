ec8ffb435d8574cdc4fffe829d55ecc1f60bee61
Shadmer <shadmer89@gmail.com>
2019-03-08 20:36:36 +0700

    Уcтановка шрифтов и начальных less стилей

---
+++ smartgrid.js
@@ -0,0 +1,43 @@
+var smartgrid = require('smart-grid');
+
+/* It's principal settings in smart grid project */
+var settings = {
+    outputStyle: 'less', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: "10px", /* gutter width px || % */
+    filename: 'smart-grid',
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            'width': '1100px', /* -> @media (max-width: 1100px) */
+            'fields': '30px' /* side fields */
+        },
+        md: {
+            'width': '960px',
+            'fields': '30px'
+        },
+        sm: {
+            'width': '780px',
+            'fields': '15px'
+        },
+        xs: {
+            'width': '560px',
+            'fields': '15px'
+        }
+        /*
+        We can create any quantity of break points.
+
+        some_name: {
+            some_width: 'Npx',
+            some_offset: 'N(px|%)'
+        }
+        */
+    },
+    oldSizeStyle: false,
+    properties: []
+};
+
+smartgrid('./src/assets/global/', settings);
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="block">
+      <div class="row">
+        <div class="col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet animi assumenda consequatur deleniti, dicta dolores eum id libero maxime nemo numquam officiis provident qui recusandae repellat sit temporibus voluptate.</div>
+        <div class="col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aliquam, nisi officia quo veritatis. Cupiditate delectus dolore doloribus facilis fugit itaque, iure labore libero quia vel veritatis vitae, voluptate.</div>
+        <div class="col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam culpa cupiditate delectus deserunt doloribus, error ex expedita fugiat iste laborum numquam omnis repellendus suscipit ut! Est natus omnis sunt?</div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  @import "./assets/fonts/font.css";
+  @import "./assets/global/normalize.css";
+  @import "./assets/global/variables.less";
+  @import "./assets/global/mixins.less";
+  @import "./assets/global/scaffolding.less";
+  @import "./assets/global/smart-grid.less";
+
+  body {
+    font-size: 18px;
+    color: #302d34;
+    line-height: 1.55;
+    font-weight: 300;
   }
-}
+
+  .block {
+    .wrapper();
+  }
+
+  .row {
+    .row-flex();
+  }
+
+  .col {
+    .col();
+    .size(4);
+  }
 </style>
