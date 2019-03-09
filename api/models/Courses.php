5260f8024204c850f22b286feb8817e663ab4103
Shancept <shancept@yandex.ru>
2019-03-09 15:07:09 +0700

    Модель курсов

---
+++ api/models/Courses.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shancept
+ * Date: 09.03.19
+ * Time: 14:43
+ */
+
+namespace models;
+
+
+use classes\Models;
+
+class Courses extends Models
+{
+    public $table_name = 'courses';
+
+    public static function getAll()
+    {
+        $sql = "SELECT * FROM `courses`";
+        $sth = self::model()->db->prepare($sql);
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    public static function getAllActive()
+    {
+        $sql = "SELECT * FROM `courses` WHERE `active` = 1";
+        $sth = self::model()->db->prepare($sql);
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    public static function getAllActiveInPage($page)
+    {
+        $sql = "SELECT * FROM `courses` WHERE `active` = 1 AND `page` = :page";
+        $sth = self::model()->db->prepare($sql);
+        $sth->bindParam(':page', $page);
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    public static function getByProductIdentifier($pi)
+    {
+        $sql = "SELECT * FROM `courses` WHERE `product_identifier` = :pi";
+        $sth = self::model()->db->prepare($sql);
+        $sth->bindParam(':pi', $pi);
+        $sth->execute();
+        return $sth->fetch();
+    }
+}
