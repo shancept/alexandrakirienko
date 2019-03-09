289b429a18f5fa7c841a96980854aa1dfdfefc7f
Shancept <shancept@yandex.ru>
2019-03-09 14:38:51 +0700

    Обновленные миграции

--- migrate.php
+++ migrate.php
@@ -32,32 +32,55 @@ $create_users_table = "CREATE TABLE `users` (
   `date_create` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-$drop_data_base = "DROP DATABASE `$db_name`";
+$create_courses_table = "CREATE TABLE `courses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `price` float NOT NULL,
+  `price_for_sale` float DEFAULT NULL,
+  `product_identifier` varchar(12) NOT NULL DEFAULT '',
+  `active` tinyint(11) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_identifier` (`product_identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+$create_feedback_table = "CREATE TABLE `feedback` (
+  `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `phone` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) DEFAULT NULL,
+  `city` VARCHAR(255) DEFAULT NULL,
+  `message` TEXT,
+  `date` DATETIME DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;";
 
-
-//Миграция для удаления базы данных
-if($argv[1] === 'drop_data_base') {
-    try {
-        $mysql->exec($drop_data_base);
-        die("Drop data base $db_name".PHP_EOL);
-    } catch (PDOException $e) {
-        die("DB ERROR: ". $e->getMessage().PHP_EOL);
-    }
-}
-
+//Создание базы данных
 try {
-    $mysql->exec($create_db_sql);
-    echo "Create data base $db_name".PHP_EOL;
+    if($mysql->exec($create_db_sql)) {
+        echo "Create data base $db_name".PHP_EOL;
+    } else {
+        echo 'error: ' . $mysql->errorInfo()[2] . PHP_EOL;
+    }
 } catch (PDOException $e) {
     die("DB ERROR: ". $e->getMessage().PHP_EOL);
 }
 
+//Создание таблиц
 $db = \classes\Db::dBConnect($config_db);
-
+$migrations = [
+    ['sql' => $create_users_table, 'message' => 'Create table users'],
+    ['sql' => $create_courses_table, 'message' => 'Create table courses'],
+    ['sql' => $create_feedback_table, 'message' => 'Create table feedback'],
+];
 try {
-    $db->exec($create_users_table);
-    echo "Create table users".PHP_EOL;
-    //todo таблица услуги
+    foreach ($migrations as $migration) {
+        if($db->exec($migration['sql'])) {
+            echo $migration['sql'] . PHP_EOL;
+        } else {
+            echo 'error: ' . $db->errorInfo()[2] . PHP_EOL;
+        }
+    }
 } catch (PDOException $e) {
     die("DB ERROR: ". $e->getMessage().PHP_EOL);
 }
