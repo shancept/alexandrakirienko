1e63dd09f56eb25b83268bd82bcc12bcd1c3d902
Shadmer <shadmer89@gmail.com>
2019-03-10 20:28:49 +0700

    Блок AlexandraKirienko

--- src/components/blocks/TextBlock.vue
+++ src/components/blocks/TextBlock.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="text-block">
         <h2 class="text-block__title">{{title}}</h2>
-        <p v-for="paragraph in text" class="text-block__text">{{paragraph}}</p>
+        <p
+                class="text-block__text"
+                v-for="paragraph in text"
+                v-html="paragraph"
+        ></p>
     </div>
 </template>
 
@@ -12,12 +16,30 @@
             'title': String,
             'text': Array
         },
-        computed: {
-
-        }
+        computed: {}
     }
 </script>
 
 <style lang="less">
+    .text-block {
+        &__title {
+            position: relative;
+            margin-bottom: 40px;
+            font-size: 32px;
 
+            &::after {
+                content: '';
+                position: absolute;
+                bottom: -10px;
+                left: 0;
+                width: 140px;
+                height: 9px;
+                border-top: 3px solid #302D34;
+                border-bottom: 3px solid #FBCFC4;
+            }
+        }
+        &__text {
+            font-size: 18px;
+        }
+    }
 </style>
--- src/components/sections/AlexandraKirienko.vue
+++ src/components/sections/AlexandraKirienko.vue
@@ -1,8 +1,23 @@
 <template>
     <section class="alexandra-kirienko">
         <div class="alexandra-kirienko__content">
-            <div class="alexandra-kirienko__row">
-                <div class="alexandra-kirienko__item  alexandra-kirienko__img-block">Картинки</div>
+            <div class="alexandra-kirienko__row  alexandra-kirienko__row--main">
+                <div class="alexandra-kirienko__item  alexandra-kirienko__img-block">
+                    <div class="alexandra-kirienko__row">
+                        <div class="alexandra-kirienko__item  alexandra-kirienko__media-1">
+                            <img src="./../../assets/img/AlexandraKirienko/image_1.jpg" alt="image_1">
+                        </div>
+                        <div class="alexandra-kirienko__item  alexandra-kirienko__media-2">
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jlbKhwV5vEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="alexandra-kirienko__item  alexandra-kirienko__media-3">
+                            <img src="./../../assets/img/AlexandraKirienko/image_2.jpg" alt="image_2">
+                        </div>
+                        <div class="alexandra-kirienko__item  alexandra-kirienko__media-4">
+                            <img src="./../../assets/img/AlexandraKirienko/image_3.png" alt="image_3">
+                        </div>
+                    </div>
+                </div>
                 <div class="alexandra-kirienko__item  alexandra-kirienko__text-block">
                     <TextBlock
                             title="Александра Кириенко"
@@ -27,7 +42,7 @@
                 text_1: [
                     'Много лет я работаю визажистом со звёздами, являюсь бьюти-экспертом у большого количества международных марок.',
                     'Самые яркие звёзды доверяют мне свои лица: Ирина Шейк, Эвелина Хромченко, Равшана Куркова, Вера Брежнева и Лена Перминова.',
-                    'Я работаю на телевидении, показах, журнальных съемках, проводила мастер-классы; являюсь make up стилистом программы "Успеть за 24 часа" на СТС с Александром Роговым и амбассадором коллекции [i]backstage line Dior[/i] в России.'
+                    'Я работаю на телевидении, показах, журнальных съемках, проводила мастер-классы; являюсь make up стилистом программы "Успеть за 24 часа" на СТС с Александром Роговым и амбассадором коллекции <i>backstage line Dior</i> в России.'
                 ]
             }
         }
@@ -43,19 +58,53 @@
         }
         &__row {
             .row-flex();
+
+            &--main {
+                justify-content: space-between;
+                align-items: center;
+
+                .md-block({ flex-direction: column; });
+            }
         }
 
         &__item {
             .col();
+            margin-bottom: 10px;
             .block();
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+
+        &__text-block {
+            .size(3.5);
+            .size-md(8);
+            .size-xs(12);
         }
 
         &__img-block {
             .size(8);
+            .size-xs(12);
         }
 
-        &__text-block {
+        &__media-1 {
+            .size(12);
+        }
+        &__media-2 {
+            .size(6);
+            .size-xs(12);
+            .xs-block({ order: 1; });
+        }
+        &__media-3 {
             .size(4);
+            .size-xs(8);
+        }
+        &__media-4 {
+            .size(2);
+            .size-xs(4);
         }
     }
 </style>
