2c8432fb90e146a62e0d82d8872e51eb32933e20
Shancept <shancept@yandex.ru>
2019-03-21 22:19:25 +0700

    404 ошибка - начало верстки

--- src/components/pages/Error404.vue
+++ src/components/pages/Error404.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Ошибка 404</h1>
+    <main class="page-content">
+        <section class="error-404">
+            <div class="error-404__container">Ошибка 404</div>
+        </section>
+    </main>
 </template>
 
 <script>
@@ -8,6 +12,19 @@
     }
 </script>
 
-<style scoped>
+<style lang="less">
+    .error-404 {
+        min-height: 700px;
+        height: 100vh;
+        background-image: url("./../../assets/img/404/404.jpg");
+        background-position: center center;
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        &__container {
+            .wrapper();
+        }
+    }
 
 </style>
