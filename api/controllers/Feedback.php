fa511fa9eb49775dd98e88634339d3f83374ab04
Shancept <shancept@yandex.ru>
2019-03-21 10:21:21 +0700

    Правка бага с передачей конфига mail

--- api/controllers/Feedback.php
+++ api/controllers/Feedback.php
@@ -39,8 +39,8 @@ class Feedback
                 ['result' => 'success'] :
                 ['result' => 'error'];
 
-            (new Mail($config, new PHPMailer(true)))
-                ->send($config['user_name'], 'Сообщение с сайта', Mail::getTextFeedBack($m_feedback));
+            (new Mail($config['mail'], new PHPMailer(true)))
+                ->send($config['mail']['user_name'], 'Сообщение с сайта', Mail::getTextFeedBack($m_feedback));
             $response->setContent(json_encode($answer));
             $response->send();
         }
